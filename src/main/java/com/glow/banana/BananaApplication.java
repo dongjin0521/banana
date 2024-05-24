d6b3fedebd085f47c025877db3814780a9ce4098
dongjin0521 <84802@naver.com>
2024-05-24 21:40:06 +0900

    스프링부트 초기 세팅 1. 그래들 사용 2. jsp 추가 3. mysql 연결

---
+++ src/main/java/com/glow/banana/BananaApplication.java
@@ -0,0 +1,13 @@
+package com.glow.banana;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BananaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BananaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/glow/banana/BananaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.glow.banana;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BananaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
